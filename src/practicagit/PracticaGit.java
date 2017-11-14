7667fa2b0c2b7e7297b170145830538ce7cc0217
pabloescoto <pabloescoto.9@gmail.com>
2017-11-14 15:56:09 -0600

    Se agrego funcion para sacar area del circulo

--- src/practicagit/PracticaGit.java
+++ src/practicagit/PracticaGit.java
@@ -5,6 +5,8 @@
  */
 package practicagit;
 
+import java.util.Scanner;
+
 /**
  *
  * @author pabloe
@@ -17,6 +19,16 @@ public class PracticaGit {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola Mundo");
+        //Area Circlo
+        Scanner sc = new Scanner(System.in);
+        double aCirculo;
+        double radio;
+        
+        System.out.println("Ingresa el radio del circulo: ");
+        radio = sc.nextDouble();
+        
+        aCirculo = Math.PI*(Math.pow(radio, 2));
+        System.out.println("El area del circulo es: " + aCirculo);
     }
     
 }
